d6d878757dee34e1092f2d35a0342e95600e91b5
Juan Delgado <juandelgado@outlook.com>
2018-07-24 18:46:27 -0500

    adding http calls with data generated and mock data part 1

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -20,6 +20,15 @@ app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
+app.get('/users', function(req, res) {
+  // Hardcoding for simplicity, but must be real database data
+  res.json([
+    {"id": 1,"firstName": "Diana","lastName": "Delgado","email": "diana.delgadod@outlook.com" },
+    {"id": 1,"firstName": "Sofia","lastName": "Delgado","email": "sofia.delgadod@outlook.com" },
+    {"id": 1,"firstName": "Alejandra","lastName": "Delgado","email": "alejandra.delgadod@outlook.com" },
+  ]);
+});
+
 app.listen(port, function(err){
   if(err){
     console.log(err);
