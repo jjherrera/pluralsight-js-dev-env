82d146572af6d64a62919953dbf84f82291c937b
Juan Delgado <juandelgado@outlook.com>
2018-07-24 19:30:27 -0500

    adding mocking data creatin and manopulation functions and libraries

--- buildScripts/generateMockData.js
+++ buildScripts/generateMockData.js
@@ -5,7 +5,7 @@
 
 /* eslint-disable no-console */
 
-import jsf from 'jason-schema-faker';
+import jsf from 'json-schema-faker';
 import {schema} from './mockDataSchema';
 import fs from 'fs';
 import chalk from 'chalk';
